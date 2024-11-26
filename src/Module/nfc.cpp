596692aa3344dbcf234b020e17143b9d20f0718e
Changes729 <changes729@163.com>
2024-11-26 16:58:48 +0800

    [NBF] support NDEF.

--- src/Module/nfc.cpp
+++ src/Module/nfc.cpp
@@ -3,16 +3,22 @@
 /* Private include -----------------------------------------------------------*/
 #include "config.h"
 
+#include <NdefMessage.h>
 #include <PN5180.h>
 #include <PN5180ISO15693.h>
 
 /* Private namespace ---------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+#define BLOCK_SIZE 16
+#define LONG_TLV_SIZE 4
+#define SHORT_TLV_SIZE 2
+
 /* Private typedef -----------------------------------------------------------*/
 /* Private template ----------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 static PN5180ISO15693 nfc(PN5180_NSS, PN5180_BUSY, PN5180_RST);
 static uint8_t _uid[8];
+static NdefMessage _current_ndef_msg;
 
 static uint8_t standardpassword[] = {0x0F, 0x0F, 0x0F, 0x0F};
 static uint8_t password[] = {0x12, 0x34, 0x56, 0x78};
@@ -20,6 +26,7 @@ static uint8_t password[] = {0x12, 0x34, 0x56, 0x78};
 /* Private class -------------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 static void _showIRQStatus(uint32_t irqStatus);
+bool decodeTlv(byte *data, int &messageLength, int &messageStartIndex);
 
 /* Private function ----------------------------------------------------------*/
 /* Private class function ----------------------------------------------------*/
@@ -68,68 +75,171 @@ bool nfc_init() {
 }
 
 void update_nfc() {
-  ISO15693ErrorCode rc = nfc.getInventory(_uid);
-  if (ISO15693_EC_OK != rc) {
-    log_e("Error in getInventory: ");
+  do {
+    /** Inventory --------------------------------------------------*/
+    ISO15693ErrorCode rc = nfc.getInventory(_uid);
+    if (ISO15693_EC_OK != rc) {
+      log_e("Error in getInventory: ");
 
-    uint32_t irqStatus = nfc.getIRQStatus();
+      uint32_t irqStatus = nfc.getIRQStatus();
 #if 0
     _showIRQStatus(irqStatus);
 #endif
-    if (0 == (RX_SOF_DET_IRQ_STAT & irqStatus)) { // no card detected
-      log_d("*** No card detected!");
+      if (0 == (RX_SOF_DET_IRQ_STAT & irqStatus)) { // no card detected
+        log_d("*** No card detected!");
+      }
+
+      nfc.reset();
+      nfc.setupRF();
+      // nfc.writeRegisterWithOrMask(0x22, 0x03);
+      // nfc.writeRegisterWithOrMask(0x27, 0x08);
+      break;
     }
 
-    nfc.reset();
-    nfc.setupRF();
-    // nfc.writeRegisterWithOrMask(0x22, 0x03);
-    // nfc.writeRegisterWithOrMask(0x27, 0x08);
-  }
-
-#if 0
-  Serial.println(F("----------------------------------"));
-  uint8_t blockSize, numBlocks;
-  rc = nfc.getSystemInfo(uid, &blockSize, &numBlocks);
-  if (ISO15693_EC_OK != rc) {
-    Serial.print(F("Error in getSystemInfo: "));
-    errorFlag = true;
-    return;
-  }
-  Serial.print(F("System Info retrieved: blockSize="));
-  Serial.print(blockSize);
-  Serial.print(F(", numBlocks="));
-  Serial.println(numBlocks);
-
-  Serial.println(F("----------------------------------"));
-  uint8_t readBuffer[blockSize];
-  for (int no = 0; no < numBlocks; no++) {
-    rc = nfc.readSingleBlock(uid, no, readBuffer, blockSize);
+    /** get sys info -----------------------------------------------*/
+    uint8_t blockSize, numBlocks;
+    rc = nfc.getSystemInfo(_uid, &blockSize, &numBlocks);
     if (ISO15693_EC_OK != rc) {
-      Serial.print(F("Error in readSingleBlock #"));
-      Serial.print(no);
-      Serial.print(": ");
-      errorFlag = true;
-      return;
+      log_e("[NFC] Error in getSystemInfo: 0x%X", rc);
+      break;
     }
-    Serial.print(F("Read block #"));
-    Serial.print(no);
-    Serial.print(": ");
-    for (int i = 0; i < blockSize; i++) {
-      if (readBuffer[i] < 16)
-        Serial.print("0");
-      Serial.print(readBuffer[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.print(" ");
-    for (int i = 0; i < blockSize; i++) {
-      if (isprint(readBuffer[i])) {
-        Serial.print((char)readBuffer[i]);
-      } else
-        Serial.print(".");
-    }
-    Serial.println();
-  }
+
+    /** read data --------------------------------------------------*/
+    uint8_t readBuffer[numBlocks * blockSize]{};
+    rc = nfc.readMultipleBlock(_uid, 0, numBlocks, readBuffer, blockSize);
+    if (ISO15693_EC_OK == rc) {
+#if 0
+      Serial.print(F("Read blocks #0-"));
+      Serial.print(numBlocks - 1);
+      Serial.println(":");
+      for (int i = 0; i < numBlocks; i++) {
+        if (i < 10)
+          Serial.print(" ");
+        Serial.print("#");
+        Serial.print(i);
+        Serial.print(": ");
+        uint16_t startAddr = i * blockSize;
+        for (int j = 0; j < blockSize; j++) {
+          if (readBuffer[startAddr + j] < 16)
+            Serial.print("0");
+          Serial.print(readBuffer[startAddr + j], HEX);
+          Serial.print(" ");
+        }
+        Serial.print(" ");
+        for (int j = 0; j < blockSize; j++) {
+          if (isprint(readBuffer[startAddr + j])) {
+            Serial.print((char)readBuffer[startAddr + j]);
+          } else
+            Serial.print(".");
+        }
+        Serial.println();
+      }
 #endif
+      int messageStartIndex = 0;
+      int messageLength = 0;
+      if (!decodeTlv(readBuffer + 4, messageLength, messageStartIndex)) {
+        _current_ndef_msg =
+            NdefMessage(readBuffer + 4 + messageStartIndex, messageLength);
+      } else {
+        _current_ndef_msg = NdefMessage();
+      }
+#if 0
+
+      Serial.println("print NdefMessage");
+      auto &message = _current_ndef_msg;
+      Serial.print("\nThis NFC Tag contains an NDEF Message with ");
+      Serial.print(message.getRecordCount());
+      Serial.print(" NDEF Record");
+      if (message.getRecordCount() != 1) {
+        Serial.print("s");
+      }
+      Serial.println(".");
+
+      // cycle through the records, printing some info from each
+      int recordCount = message.getRecordCount();
+      for (int i = 0; i < recordCount; i++) {
+        Serial.print("\nNDEF Record ");
+        Serial.println(i + 1);
+        NdefRecord record = message.getRecord(i);
+        // NdefRecord record = message[i]; // alternate syntax
+
+        Serial.print("  TNF: ");
+        Serial.println(record.getTnf());
+        Serial.print("  Type: ");
+        Serial.println(record.getType()); // will be "" for TNF_EMPTY
+
+        // The TNF and Type should be used to determine how your application
+        // processes the payload There's no generic processing for the
+        // payload, it's returned as a byte[]
+        int payloadLength = record.getPayloadLength();
+        byte payload[payloadLength];
+        record.getPayload(payload);
+
+        // Print the Hex and Printable Characters
+        Serial.print("  Payload (HEX): ");
+        PrintHexChar(payload, payloadLength);
+
+        // Force the data into a String (might work depending on the content)
+        // Real code should use smarter processing
+        String payloadAsString = "";
+        for (int c = 0; c < payloadLength; c++) {
+          payloadAsString += (char)payload[c];
+        }
+        Serial.print("  Payload (as String): ");
+        Serial.println(payloadAsString);
+
+        // id is probably blank and will return ""
+        String uid = record.getId();
+        if (uid != "") {
+          Serial.print("  ID: ");
+          Serial.println(uid);
+        }
+      }
+#endif
+    } else {
+      log_e("[NFC] read multiple block failed: 0x%X", rc);
+      break;
+    }
+  } while (0);
+}
+
+int getNdefStartIndex(byte *data) {
+
+  for (int i = 0; i < BLOCK_SIZE; i++) {
+    if (data[i] == 0x0) {
+      // do nothing, skip
+    } else if (data[i] == 0x3) {
+      return i;
+    } else {
+#ifdef NDEF_USE_SERIAL
+      log_e("Unknown TLV 0x%X", data[i]);
+#endif
+      return -2;
+    }
+  }
+
+  return -1;
+}
+
+bool decodeTlv(byte *data, int &messageLength, int &messageStartIndex) {
+  int i = getNdefStartIndex(data);
+
+  if (i < 0 || data[i] != 0x3) {
+#ifdef NDEF_USE_SERIAL
+    log_e("Error. Can't decode message length.");
+#endif
+    return false;
+  } else {
+    if (data[i + 1] == 0xFF) {
+      messageLength = ((0xFF & data[i + 2]) << 8) | (0xFF & data[i + 3]);
+      messageStartIndex = i + LONG_TLV_SIZE;
+    } else {
+      messageLength = data[i + 1];
+      messageStartIndex = i + SHORT_TLV_SIZE;
+    }
+  }
+
+  return true;
 }
 
 bool has_uid(uint8_t *buffer, size_t size) {
